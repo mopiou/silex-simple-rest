c36baded17bfc06fa6bab7f6a175ed34a90a8fa8
mopiou <briansoufir4@hotmail.com>
2016-07-19 16:42:22 +0200

    a

--- web/index.php
+++ web/index.php
@@ -1,22 +1,17 @@
 <?php
 
-echo 'test 1 ';
 require_once __DIR__ . '/../vendor/autoload.php';
-
-echo 'test 2 ';
-
 
 
 $app = new Silex\Application();
 
-echo 'test 3 ';
 
 require __DIR__ . '/../resources/config/prod.php';
 
-echo 'test 4 ';
 
-/*
 
 require __DIR__ . '/../src/app.php';
+
+echo 'test 3 ';
 
 $app['http_cache']->run();
