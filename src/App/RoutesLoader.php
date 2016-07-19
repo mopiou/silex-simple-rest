cd9627e60bc96e07e4735b3c2ff28fa3a6bd0d7d
mopiou <briansoufir4@hotmail.com>
2016-07-19 18:11:26 +0200

    a

--- src/App/RoutesLoader.php
+++ src/App/RoutesLoader.php
@@ -31,7 +31,7 @@ class RoutesLoader
 
         $api = $this->app["controllers_factory"];
 
-        $api->get('/notes', "notes.controller:getAll");
+        $api->get('/api/notes', "notes.controller:getAll");
         $api->post('/notes', "notes.controller:save");
         $api->put('/notes/{id}', "notes.controller:update");
         $api->delete('/notes/{id}', "notes.controller:delete");
