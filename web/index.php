201a1dfa504e76a9caecf0a9cf997936fa7f5773
mopiou <briansoufir4@hotmail.com>
2016-07-19 13:46:13 +0200

    a

--- web/index.php
+++ web/index.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-require __DIR__ . '/../resources/config/prod.php';
+//require __DIR__ . '/../resources/config/prod.php';
 
-require __DIR__ . '/../src/app.php';
+//require __DIR__ . '/../src/app.php';
 
 echo 'test';
 
