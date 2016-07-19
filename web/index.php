6f3e3bae06aacf476982ae58298234de7f16774a
mopiou <briansoufir4@hotmail.com>
2016-07-19 16:36:02 +0200

    a

--- web/index.php
+++ web/index.php
@@ -1,13 +1,19 @@
 <?php
 
-echo 'test brian ';
+echo 'test 1 ';
 require_once __DIR__ . '/../vendor/autoload.php';
+
+echo 'test 2 ';
 
 
 
 $app = new Silex\Application();
 
+echo 'test 3 ';
+
 require __DIR__ . '/../resources/config/prod.php';
+
+echo 'test 4 ';
 
 /*
 
