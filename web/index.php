502288360761695c5fee2aa860a96501b39d1ac3
mopiou <briansoufir4@hotmail.com>
2016-07-19 15:56:52 +0200

    a

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
 <?php
 
-require_once  'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-echo 'test';
+$app = new Silex\Application();
+
+require __DIR__ . '/../resources/config/dev.php';
+
+require __DIR__ . '/../src/app.php';
+
+$app['http_cache']->run();
