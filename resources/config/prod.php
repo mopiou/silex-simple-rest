10916e77f4fbc187d4df3a2bf7df2a72fc24cc99
mopiou <briansoufir4@hotmail.com>
2016-07-19 17:53:15 +0200

    a

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -2,10 +2,23 @@
 $app['log.level'] = Monolog\Logger::ERROR;
 $app['api.version'] = "v1";
 $app['api.endpoint'] = "/api";
-$app['db.options'] = array(
-  "driver" => "pdo_mysql",
-  "user" => "root",
-  "password" => "root",
-  "dbname" => "testSilex",
-  "host" => "localhost",
+
+
+
+$app['db.options'] = array (
+   'driver'    => 'pdo_mysql',
+   'host'      => 'azakinechttest.mysql.db',
+   'dbname'    => 'azakinechttest',
+   'user'      => 'azakinechttest',
+   'password'  => 'Testtest01',
+   'charset'   => 'utf8',
 );
+
+
+// $app['db.options'] = array(
+//   "driver" => "pdo_mysql",
+//   "user" => "root",
+//   "password" => "root",
+//   "dbname" => "testSilex",
+//   "host" => "localhost",
+// );
