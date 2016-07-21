691a8b688948ff1bc9220de3e9996fa3f83df3cc
anthonyzakine <anthonyzakine@gmail.com>
2016-07-21 16:44:21 +0200

    c

--- src/App/Controllers/NotesController.php
+++ src/App/Controllers/NotesController.php
@@ -54,15 +54,16 @@ class NotesController
      public function teste_connection(Request $request){
 
         //$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
-        var_dump($request->get('id_user'));
-        //$request->request->get("id_user");
+        //var_dump($request->get('id_user'));
+        $email =$request->request->get("email");
+        $password =$request->request->get("password");
         //$request->request->get('form_name')['name'];
 
         //var_dump($request->request);
             //die(var_dump($request->request->get('name')));
 
 
-        //return new JsonResponse($this->notesService->teste_connection($email,$password));
+        return new JsonResponse($this->notesService->teste_connection($email,$password));
 
        // return new JsonResponse(array("id" => $this->notesService->save($note)));
 
