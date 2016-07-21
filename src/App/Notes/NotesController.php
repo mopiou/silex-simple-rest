ca56172ff11b15209471b3db4a696b3508b13cf8
mopiou <briansoufir4@hotmail.com>
2016-07-21 14:58:03 +0200

    a

--- src/App/Notes/NotesController.php
+++ src/App/Notes/NotesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Notes;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
--- src/App/Notes/NotesRoutesLoader.php
+++ src/App/Notes/NotesRoutesLoader.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Notes;
 
 use Silex\Application;
 
