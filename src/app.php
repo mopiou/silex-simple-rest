13ddb03079704dfff87996c83e88c2587ba94abd
anthonyzakine <anthonyzakine@gmail.com>
2016-07-25 11:49:54 +0200

    a

--- src/app.php
+++ src/app.php
@@ -95,6 +95,61 @@ $app->register(new Gigablah\Silex\OAuth\OAuthServiceProvider(), array(
 
 
 
+// Provides URL generation
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+// Provides CSRF token generation
+$app->register(new Silex\Provider\FormServiceProvider());
+// Provides session storage
+$app->register(new Silex\Provider\SessionServiceProvider(), array(
+    'session.storage.save_path' => __DIR__ . '/../cache'
+));
+// Provides Twig template engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__
+));
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'default' => array(
+            'pattern' => '^/',
+            'anonymous' => true,
+            'oauth' => array(
+                //'login_path' => '/auth/{service}',
+                //'callback_path' => '/auth/{service}/callback',
+                //'check_path' => '/auth/{service}/check',
+                'failure_path' => '/',
+                'with_csrf' => true
+            ),
+            'logout' => array(
+                'logout_path' => '/logout',
+                'with_csrf' => true
+            ),
+            'users' => new Gigablah\Silex\OAuth\Security\User\Provider\OAuthInMemoryUserProvider()
+        )
+    ),
+    'security.access_rules' => array(
+        array('^/auth', 'ROLE_USER')
+    )
+));
+$app->before(function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    $token = $app['security']->getToken();
+    $app['user'] = null;
+    if ($token && !$app['security.trust_resolver']->isAnonymous($token)) {
+        $app['user'] = $token->getUser();
+    }
+});
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'login_paths' => $app['oauth.login_paths'],
+        'logout_path' => $app['url_generator']->generate('logout', array(
+            '_csrf_token' => $app['oauth.csrf_token']('logout')
+        ))
+    ));
+});
+$app->match('/logout', function () {
+})->bind('logout');
+
+
+
 
 $app->register(new ServiceControllerServiceProvider());
 
