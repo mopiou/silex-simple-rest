18a4f4f4b5f2ffd617bab66d76d6b6b5fc63709f
anthonyzakine <anthonyzakine@gmail.com>
2016-07-25 11:48:33 +0200

    a

--- src/app.php
+++ src/app.php
@@ -19,6 +19,42 @@ define("ROOT_PATH", __DIR__ . "/..");
 
 
 
+
+
+
+
+//handling CORS preflight request
+$app->before(function (Request $request) {
+   if ($request->getMethod() === "OPTIONS") {
+       $response = new Response();
+       $response->headers->set("Access-Control-Allow-Origin","*");
+       $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+       $response->headers->set("Access-Control-Allow-Headers","Content-Type");
+       $response->setStatusCode(200);
+       return $response->send();
+   }
+}, Application::EARLY_EVENT);
+
+
+
+//handling CORS respons with right headers
+$app->after(function (Request $request, Response $response) {
+   $response->headers->set("Access-Control-Allow-Origin","*");
+   $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+});
+
+//accepting JSON
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+
+
+
+
 $app->register(new Gigablah\Silex\OAuth\OAuthServiceProvider(), array(
     'oauth.services' => array(
         'Facebook' => array(
@@ -57,37 +93,6 @@ $app->register(new Gigablah\Silex\OAuth\OAuthServiceProvider(), array(
     )
 ));
 
-
-
-
-
-//handling CORS preflight request
-$app->before(function (Request $request) {
-   if ($request->getMethod() === "OPTIONS") {
-       $response = new Response();
-       $response->headers->set("Access-Control-Allow-Origin","*");
-       $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-       $response->headers->set("Access-Control-Allow-Headers","Content-Type");
-       $response->setStatusCode(200);
-       return $response->send();
-   }
-}, Application::EARLY_EVENT);
-
-
-
-//handling CORS respons with right headers
-$app->after(function (Request $request, Response $response) {
-   $response->headers->set("Access-Control-Allow-Origin","*");
-   $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-});
-
-//accepting JSON
-$app->before(function (Request $request) {
-    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-        $data = json_decode($request->getContent(), true);
-        $request->request->replace(is_array($data) ? $data : array());
-    }
-});
 
 
 
