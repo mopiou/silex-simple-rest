a4bcc0f6e50db576a8b3648889ce0249e76d0ac8
anthonyzakine <anthonyzakine@gmail.com>
2016-07-26 12:19:38 +0200

    changement

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -2,10 +2,23 @@
 $app['log.level'] = Monolog\Logger::ERROR;
 $app['api.version'] = "v1";
 $app['api.endpoint'] = "/api";
-$app['db.options'] = array(
-  "driver" => "pdo_mysql",
-  "user" => "root",
-  "password" => "root",
-  "dbname" => "prod_db",
-  "host" => "prod_host",
+
+
+
+$app['db.options'] = array (
+   'driver'    => 'pdo_mysql',
+   'host'      => 'azakinechttest.mysql.db',
+   'dbname'    => 'azakinechttest',
+   'user'      => 'azakinechttest',
+   'password'  => 'Testtest01',
+   'charset'   => 'utf8',
 );
+
+
+// $app['db.options'] = array(
+//   "driver" => "pdo_mysql",
+//   "user" => "root",
+//   "password" => "root",
+//   "dbname" => "testSilex",
+//   "host" => "localhost",
+// );
