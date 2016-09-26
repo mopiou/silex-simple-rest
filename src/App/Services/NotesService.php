0db4fe226d8557e1d08bbaf998e24cc7cbf775c7
anthonyzakine <anthonyzakine@gmail.com>
2016-09-26 18:36:08 +0200

    f

--- src/App/Services/NotesService.php
+++ src/App/Services/NotesService.php
@@ -19,6 +19,9 @@ class NotesService extends BaseService
 
         // return $note;
 
+
+        //SOLUTION mais renvoie null et ajout null en argument en base
+
         $data = array(
         'id_note' => '$id_note',
         'note' => '$note'
@@ -27,12 +30,16 @@ class NotesService extends BaseService
         $this->db->insert('notes', $data);
         return ;
 
+        //fin solution
+
         $result =$this->db->insert_batch($data);
         //$this->db->insert("notes", $id_note,$note);
         //$this->db->insert("notes", $note);
         //$this->db->insert('notes', ['id_note' => $id_note],['note' => $note]);
         return $result;
 
+
+        //marche mais c'est de la merde
 
         //$requete= $this->db->query('INSERT INTO notes VALUES ("'.$id_note.'","'.$note.'")');
         
