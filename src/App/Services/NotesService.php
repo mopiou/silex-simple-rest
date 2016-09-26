dab8aeee64befff9292d4f08849ac093406fe63d
anthonyzakine <anthonyzakine@gmail.com>
2016-09-26 16:50:04 +0200

    d

--- src/App/Services/NotesService.php
+++ src/App/Services/NotesService.php
@@ -20,7 +20,7 @@ class NotesService extends BaseService
 
         $requete= $this->db->prepare("INSERT INTO notes VALUES (?,?)");
         $requete->execute(array($id_note,$note));
-        $requete = $requete->$query->fetch();
+        $requete =$query->fetch();
        
         return $requete;
         //return $this->db->lastInsertId();
