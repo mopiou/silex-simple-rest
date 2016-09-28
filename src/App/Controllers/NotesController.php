b628a025b9695ec50478d2d9b0c1fd2291c4adb2
anthonyzakine <anthonyzakine@gmail.com>
2016-09-28 17:28:17 +0200

    f

--- src/App/Controllers/NotesController.php
+++ src/App/Controllers/NotesController.php
@@ -44,8 +44,8 @@ class NotesController
         }
 
         if ($new_note <8) {
-            $app->abort(404, "Post $id does not exist.");
-            //return $app->error('Parametere Negatif: note', 400);
+            //$app->abort(404, "Post $id does not exist.");
+            $app->error('Parametere Negatif: note', 400);
            // return $app->json('Parametere Negatif: note', 400);
         }
 
