23318e52335957cf4123332e2ee24d08351cbf75
anthonyzakine <anthonyzakine@gmail.com>
2016-09-29 16:42:01 +0200

    c

--- src/App/Services/UsersService.php
+++ src/App/Services/UsersService.php
@@ -10,27 +10,27 @@ class UsersService extends BaseService
         return $this->db->fetchAll("SELECT * FROM user");
     }
 
-    function create($prenom,$nom,$email,$id_genre,$age,$description,$password,$ville,$date_inscription,$super_like)
-    {
+    // function create($prenom,$nom,$email,$id_genre,$age,$description,$password,$ville,$date_inscription,$super_like)
+    // {
         
-        $data = array(
-        'id_user' => default,
-        'prenom' => $prenom,
-        'nom' => $nom,
-        'email' => $email,
-        'id_genre' => $id_genre,
-        'age' => $age,
-        'description' => $description,
-        'password' => $password,
-        'ville' => $ville,
-        'date_inscription' => $date_inscription,
-        'super_like' => $super_like
-        );
+    //     $data = array(
+    //     'id_user' => default,
+    //     'prenom' => $prenom,
+    //     'nom' => $nom,
+    //     'email' => $email,
+    //     'id_genre' => $id_genre,
+    //     'age' => $age,
+    //     'description' => $description,
+    //     'password' => $password,
+    //     'ville' => $ville,
+    //     'date_inscription' => $date_inscription,
+    //     'super_like' => $super_like
+    //     );
 
-        $this->db->insert('user', $data);
-        return $data;
+    //     $this->db->insert('user', $data);
+    //     return $data;
       
-    }
+    // }
 
     function update($id_note, $note)
     {
