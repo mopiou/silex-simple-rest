b86c83fd2f8ad4cb5d8a043f5ca1c17feee71f18
anthonyzakine <anthonyzakine@gmail.com>
2016-09-30 15:05:43 +0200

    d

--- src/App/Services/UsersService.php
+++ src/App/Services/UsersService.php
@@ -13,8 +13,9 @@ class UsersService extends BaseService
 
     public function getUser($id_user)
     {
-        return $this->db->select("SELECT * FROM user where id_user=".$id_user);
-       //return $this->db->getwhere('user', array('id_user' => $id_user));    
+        //return $this->db->fetchAll("SELECT * FROM user where id_user=".$id_user);
+       //return $this->db->getwhere('user', array('id_user' => $id_user));   
+      return $this->db->get("user", array("id_user" => $id_user)); 
     }
 
     function create($prenom,$nom,$email,$id_genre,$age,$description,$password,$ville,$date_inscription,$super_like)
